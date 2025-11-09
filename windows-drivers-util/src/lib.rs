5180feb74de71522ca1001e174105235e1bf3185
Jonas Zaddach <5988756+zaddach@users.noreply.github.com>
2025-11-09 18:31:23 +0100

    Add kd_print! macro

--- windows-drivers-util/src/lib.rs
+++ windows-drivers-util/src/lib.rs
@@ -4,6 +4,33 @@ use wdk_sys::{
    PIO_STACK_LOCATION, PIRP, MDL, MDL_MAPPED_TO_SYSTEM_VA, MDL_SOURCE_IS_NONPAGED_POOL,
 };
 
+/// Macro to print debug messages to the kernel debugger.
+/// Courtesy of mhandb: https://github.com/microsoft/windows-drivers-rs/discussions/17
+#[cfg(debug_assertions)]
+#[macro_export]
+macro_rules! kd_print {
+    ($msg: expr) => {
+        #[allow(unused_unsafe)]
+        unsafe {
+            DbgPrint(concat!($msg, "\0").as_ptr() as *mut i8)
+        }
+    };
+
+    ($format: expr, $($arg:tt)*) => {
+        #[allow(unused_unsafe)]
+        unsafe {
+             DbgPrint(concat!($format, "\0").as_ptr() as *mut i8, $($arg)*)
+        }
+    };
+}
+
+#[cfg(not(debug_assertions))]
+#[macro_export]
+macro_rules! kd_print {
+    ($msg: expr) => {};
+    ($format: expr, $($arg:tt)*) => {};
+}
+
 /// This routine is invoked to return a pointer to the current stack location
 /// in an I/O Request Packet (IRP).
 ///
